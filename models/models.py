f359efa625dca451aa780923b000c5c051a50a0d
2dam3 <2dam3@hz114470.H012108.NET>
2021-01-28 12:13:50 +0100

    vistas

--- models/models.py
+++ models/models.py
@@ -19,41 +19,42 @@ from odoo import models, fields, api
 
 
 class asignaturas(models.Model):
-    _name= 'asignaturas'
+    _name= 'asignaturas.asignatura'
 
     name= fields.Char(string='nombre')
     profesor = fields.Char(string='profesor')
-    ciclo = fields.Char(string='ciclo')
-    id = fields.Id()
+    ciclo = fields.Many2one(string='ciclos.ciclo')
+
+
 
 
 class ciclos(models.Model):
-    _name='ciclos.ciclos'
+    _name='ciclos.ciclo'
 
     name=fields.Char(string='ciclos')
-    ciclos_id = fields.Id()
-    aula=fields.Many2one('aulas.aula','aula_id')
-    
+    aula=fields.Many2one('aulas.aula','ciclo')
+    asignatura=fields.One2Many('asignaturas.asignatura','ciclos',string="ciclos")
+
 class alumnos(models.model):
     _name='alumnos.alumno'
 
-    alumnos_id=fields.id();
     nombre=fields.Char(string='nombre')
     apellido=fields.Char(string='nombre')
-    ordenadores=fields.Many2one('ordenadores.ordenador','ordenadores_id',required=True)
+    ordenadores=fields.Many2one('ordenadores.ordenador','alumnos',required=True)
+    faltas=fields.Int(String="faltas")
+    nota=fields.Float(String="notas")
 
 
 class ordenadores(models.Model):
     _name='ordenadores.ordenador'
      
-    ordenadores_id=fields.id();
     ocupado=fields.Selection( [('Ocupado', 'Ocupado'),('Libre', 'Libre'),('Reparando','Reparando')],'Asignacion', default='Libre')
-    alumnos=fields.One2many('alumnos','alumnos_id')
-    aula=fields.One2many('aulas.aula','aula_id')
+    alumnos=fields.One2many('alumnos.alumno','ordenadores')
+    aula=fields.One2many('aulas.aula','ordenadores')
 
 class aulas():
     _name='aulas.aula'
 
-    aula_id=fields.id();
-    nombre=fields.Char(string='nombre');
-    ciclo=fields.one2many('ciclos.ciclo','ciclos_id')
+    nombre=fields.Char(string='nombre')
+    ciclo=fields.one2many('ciclos.ciclo')
+    ordenadores=fields.Many2One('ordenadores.ordenador','aula')
